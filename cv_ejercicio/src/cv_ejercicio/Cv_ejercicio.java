e365c6a4f2e87d4c2aab3bb5b7ec06bc76da4284
Jhoane-Black <jhoaneblack@hotmail.com>
2020-11-06 17:51:37 -0500

    mezclado desde master

--- cv_ejercicio/src/cv_ejercicio/Cv_ejercicio.java
+++ cv_ejercicio/src/cv_ejercicio/Cv_ejercicio.java
@@ -19,6 +19,7 @@ public class Cv_ejercicio {
         System.out.println("version 0");
         System.out.println("version 1.0");
         System.out.println("version 1.1");
+        System.out.println("mezclado");
     }
     
 }
